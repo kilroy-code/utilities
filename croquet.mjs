50c3845b9ff6f403488bdd0d2022698b1107d4d7
Howard (Aech Linden) Stearns <howard.stearns@gmail.com>
2022-12-11 20:45:03 -0800

    Reference crqouet-in-memory property.

--- croquet.mjs
+++ croquet.mjs
@@ -1,4 +1,4 @@
 // Subtle and fagile timing!
 // In the browser, everything gets bound synchronously, using the window global, which we include in the header, so it's ready by the time this gets loaded.
 // In NodeJS, it's bound right away, but doesn't have a value until the await import completes, which HAPPENS to be faster than the binding is used because it's not going through a server.
-export const { Croquet } = (typeof window !== 'undefined') ? window : await import('../croquet-in-memory/index.mjs');
+export const { Croquet } = (typeof window !== 'undefined') ? window : await import('@kilroy-code/croquet-in-memory/index.mjs');
